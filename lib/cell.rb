4b59a5aac82d9a0d8a4f9d10d1bc58be0fb955e5
Brad Breiten <jonathan@breiten.us>
2021-10-18 22:26:13 -0600

    Create cell class and test initialize

---
+++ lib/cell.rb
@@ -0,0 +1,13 @@
+class Cell
+
+  attr_reader :position
+
+  def initialize(position)
+
+    @position = position
+    
+
+  end
+
+
+end
---
+++ spec/cell_spec.rb
@@ -0,0 +1,24 @@
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do
+  before(:each) do
+    @cell = Cell.new("B4")
+  end
+
+  describe '#initialize' do
+    it 'creates instance' do
+      expect(@cell).to be_instance_of Cell
+    end
+
+    it 'has positional coordinate' do
+      expect(@cell.position).to eq("B4")
+    end
+
+
+  end
+
+
+
+
+end
