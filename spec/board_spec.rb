5d6e35fe8c8fb4f46469c1a7cecb4d5372f65042
John Napier <jcee.napier@gmail.com>
2021-10-18 20:17:41 -0600

    Add test to show proper ship placement for valid placement method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -55,4 +55,13 @@ describe 'validating ship placements' do
     expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be(false)
     expect(board.valid_placement?(submarine, ["C2", "D3"])).to be(false)
   end
+
+  it 'can succesffully place ships on the board' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(submarine, ["A1", "A2"])).to be(true)
+    expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be(true)
+  end
 end
