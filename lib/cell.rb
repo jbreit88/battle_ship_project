f2d00d6d44efe3a62c00e2668e275480ac374bce
Brad Breiten <jonathan@breiten.us>
2021-10-18 22:41:40 -0600

    Add ship and empty? method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,11 +1,23 @@
 class Cell
 
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
 
   def initialize(coordinate)
 
     @coordinate = coordinate
-    
+    @empty = true
+  end
+
+  def ship
+    @ship
+  end
+
+  def empty?
+    @empty
+  end
+
+  def place_ship(ship_name, ship_length)
+
 
   end
 
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -14,8 +14,18 @@ RSpec.describe Cell do
     it 'has positional coordinate' do
       expect(@cell.coordinate).to eq("B4")
     end
+  end
 
+  describe '#ship' do
+    it 'has no ship' do
+      expect(@cell.ship).to eq nil
+    end
+  end
 
+  describe '#empty?' do
+    it 'is empty' do
+      expect(@cell.empty?).to be true
+    end
   end
 
 
