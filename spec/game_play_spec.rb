3ed96d113ffd80fa9ce021e4a8f8b2c2129eaf12
Brad Breiten <jonathan@breiten.us>
2021-10-22 15:35:39 -0600

    Add GamePlay spec file
    Co-authored-by: John <jcee.napier@gmail.com>

---
+++ spec/game_play_spec.rb
@@ -0,0 +1,23 @@
+require "rspec"
+require './lib/ship'
+require "./lib/board"
+require './lib/ship'
+require './lib/cell'
+require './lib/game_play.rb'
+
+RSpec.describe GamePlay do #John's Tests
+
+
+
+
+
+end
+
+
+RSpec.describe GamePlay do #Brad's Tests
+
+
+
+
+
+end
