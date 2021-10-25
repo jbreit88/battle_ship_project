ca4d3b085cc0cd26cbf5150b38005635506e917d
Brad Breiten <jonathan@breiten.us>
2021-10-24 18:07:36 -0600

    Update player class

---
+++ lib/player.rb
@@ -0,0 +1,32 @@
+class Player
+
+  def initialize
+
+    @player_board = Board.new
+
+  end
+
+
+  # def player_set_board_render # Shows board that player has set
+  #   puts "Show ship placement? Y or N"
+  #   answer = gets.chomp.downcase
+  #
+  #   until ["y", "n"].include?(answer)
+  #     puts "Invalid input. Please try again:"
+  #     answer = gets.chomp.downcase
+  #   end
+  # end
+  #
+  # if answer == "y"
+  #   @player_board.render(true)
+  #
+  # elsif answer == "n"
+  #   @player_board.render
+  # end
+
+
+
+
+
+
+end
