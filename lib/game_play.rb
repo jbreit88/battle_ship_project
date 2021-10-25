0e247387391457c86437ecdedc9d7c8120f368f0
Brad Breiten <jonathan@breiten.us>
2021-10-24 23:40:19 -0600

    Add: add methods to play game

--- lib/game_play.rb
+++ lib/game_play.rb
@@ -62,26 +62,110 @@ class GamePlay
     sleep(1.5)
     puts "Enter your desired coordinates for the Submarine (2 spaces):"
     @player.player_ship_place_submarine
+    puts "Player ships placed!"
+    sleep(2.0)
   end
 
   def turn
     until @player.all_sunk? == true || @computer.all_sunk? == true
+      puts "These are the boards:"
+      sleep(1.5)
       puts "#{"=" * 10} COMPUTER BOARD #{"=" * 10}"
       print @computer.cpu_display_board
-      puts "#{"=" * 10} COMPUTER BOARD #{"=" * 10}"
-      print @player.player_board_render # Make sure this is rendering ship locations AND all other information.
-      sleep(1.5)
+      puts "#{"=" * 10} PLAYER BOARD #{"=" * 10}"
+      print @player.player_board_render
+      sleep(2.0)
       puts "Player shot. Input cell to be fired upon:"
       @computer.player_shot
-
+      sleep(1.5)
+      puts "Computer is choosing point of attack."
+      sleep(1.5)
+      @player.comp_shot
     end
   end
 
-  def results
-
-  end
-
   def end_game
+    if @computer.all_sunk? && @player.all_sunk?
+      puts "Ending boards are:"
+      puts "#{"=" * 10} COMPUTER BOARD #{"=" * 10}"
+      print @computer.cpu_display_board
+      puts "#{"=" * 10} PLAYER BOARD #{"=" * 10}"
+      print @player.player_board_render
+      sleep(1.5)
+      puts "It's a draw. Would you like to play again? Y or N"
+      player_input = gets.chomp.downcase
 
+      until ["y", "n"].include?(player_input)
+        puts "Not a valid selection, Please try again:"
+        player_input = gets.chomp.downcase
+      end
+
+      if player_input == "y"
+        puts "Let's try it again!"
+      elsif player_input == "n"
+        puts "Thank you for playing!"
+        sleep(1.5)
+        puts "Goodbye!"
+        sleep(1.5)
+        exit(true)
+      end
+
+    elsif @computer.all_sunk? && @player.all_sunk? == false
+      puts "Ending boards are:"
+      puts "#{"=" * 10} COMPUTER BOARD #{"=" * 10}"
+      print @computer.cpu_display_board
+      puts "#{"=" * 10} PLAYER BOARD #{"=" * 10}"
+      print @player.player_board_render
+      sleep(1.5)
+      puts "Congrats! You showed that computer what for!"
+      sleep(1.5)
+      puts "Would you like to play again? Y or N"
+
+      player_input = gets.chomp.downcase
+
+      until ["y", "n"].include?(player_input)
+        puts "Not a valid selection, Please try again:"
+        player_input = gets.chomp.downcase
+      end
+
+      if player_input == "y"
+        puts "Let's try it again!"
+      elsif player_input == "n"
+        puts "Thank you for playing!"
+        sleep(1.5)
+        puts "Goodbye!"
+        sleep(1.5)
+        exit(true)
+      end
+
+
+    elsif @player.all_sunk? && @computer.all_sunk? == false
+      puts "Ending boards are:"
+      puts "#{"=" * 10} COMPUTER BOARD #{"=" * 10}"
+      print @computer.cpu_display_board
+      puts "#{"=" * 10} PLAYER BOARD #{"=" * 10}"
+      print @player.player_board_render
+      sleep(1.5)
+      puts "Looks like the computer took it this time."
+      sleep(1.5)
+      puts "Would you like to try again? Y or N"
+
+      player_input = gets.chomp.downcase
+
+      until ["y", "n"].include?(player_input)
+        puts "Not a valid selection, Please try again:"
+        player_input = gets.chomp.downcase
+      end
+
+      if player_input == "y"
+        puts "Let's try it again!"
+      elsif player_input == "n"
+        puts "Thank you for playing!"
+        sleep(1.0)
+        puts "Goodbye!"
+        sleep(1.5)
+        exit(true)
+      end
+    end
   end
 end
